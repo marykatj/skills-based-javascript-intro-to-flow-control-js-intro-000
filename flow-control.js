48162461278e951523c99198256d84f9c153683d
Mary-Kate James <marykatj@gmail.com>
2018-04-04 15:37:52 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,17 @@
+<<<<<<< HEAD
 var basicTeenager = function(age) {
 if (age >= 13 && age <= 19) {
 return "You are a teenager!";
+=======
+function basicTeenager(age) {
+if (13<=age<=19)
+return "You are a teenager!"
+>>>>>>> d77f94cfc0893b899e8d938bdb72495331ae5189
 }
 return undefined;
 };
 
+<<<<<<< HEAD
 
 var teenager = function (age) {
   if(age >= 13 && age <= 19) {
@@ -21,6 +28,31 @@ return "You are a teenager!";
 return "You are a kid";
 } else {
 return "You are a grownup";
+=======
+function teenager(age) {
+  if(13<=age<=19)
+  return "You are a teenager!"
+} else {
+return "You are not a teenager"
+}
+
+function ageChecker(age) {
+if (13<=age<=19) {
+return "You are a teenager!"
+} else if (age < 13) {
+return "You are a kid"
+} else if (age > 19) {
+return "You are a grownup"
+}
+}
+
+function ternaryTeenager(age) {
+
+}
+
+function switchAge(age) {
+
+>>>>>>> d77f94cfc0893b899e8d938bdb72495331ae5189
 }
 };
 
